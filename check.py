e821ecf35ade9dd5e229c39571e32d13c8f79f53
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-06 22:29:23 +0545

    Update check.py

--- check.py
+++ check.py
@@ -1,13 +1,12 @@
-import requests
-
-proxy_url = 'https://raw.githubusercontent.com/Ramxantanha/data/main/proxies.txt'
-
-try:
-    response = requests.get(proxy_url)
-    if response.status_code == 200:
-        proxies = response.text.strip().split('\n')
-        print(proxies)
-    else:
-        print("Failed to fetch proxies from the provided URL.")
-except requests.RequestException as e:
-    print("An error occurred:", e)
+def get_proxies(proxy_url):
+    try:
+        response = requests.get(proxy_url)
+        if response.status_code == 200:
+            proxies = response.text.strip().split('\n')
+            return proxies
+        else:
+            print("Failed to fetch proxies from the provided URL.")
+            return []
+    except requests.RequestException as e:
+        print("An error occurred while fetching proxies:", e)
+        return []
